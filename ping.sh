ec31fc9b80cae50310a18ae8987b8569733f2891
HackerGimang <113837274+HackerGimang@users.noreply.github.com>
2022-09-18 19:15:22 +0700

    Create ping.sh

---
+++ ping.sh
@@ -0,0 +1,52 @@
+clear
+cyan='\033[36;1m'
+red='\033[31;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+white='\033[37;1m'
+yellow='\033[1;33m'
+sleep 1
+echo
+toilet -f big -F gay Script
+date
+echo
+sleep 2
+echo
+echo $white"["$green"1"$white"]"$red"Google"
+echo
+echo $white"["$green"2"$white"]"$red"CloudFlare"
+echo
+echo $white"["$green"3"$white"]"$red"Keluar"
+echo
+echo $cyan
+read -p "Pilih Mana : " bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+toilet -f big -F gay Google
+echo $cyan"Stabilkan Jaringan"
+sleep 2
+echo
+ping -s9000 8.8.8.8
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+toilet -f big -F gay CloudFlare
+echo $cyan"Stabilkan Jaringan"
+sleep 2
+echo
+ping -s9000 1.1.1.1
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+echo $cyan
+figlet "Dadah"
+echo $cyan "BRUHHH"
+sleep 3
+login
+fi
